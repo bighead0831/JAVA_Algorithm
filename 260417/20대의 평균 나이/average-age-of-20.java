80fb1be7488264caf62db8860c9661556c6a145e
Gomin <bighead0831@naver.com>
2026-04-17 23:53:38 +0900

    Title: 20대의 평균 나이, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260417/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int cnt = 0;
+        while(num>=20 && num<30) {
+            if(num>=30 && num<20) continue;
+            else {
+                sum += num;
+                cnt++;
+            }
+            num = Integer.parseInt(br.readLine());
+        }
+        double avg = (double)sum/cnt;
+        System.out.printf("%.2f", avg);
+
+    }
+}
