1e62fcca508f9a97b9189d2aa0b49fb6994819e5
Max Kim <bighead0831@naver.com>
2026-09-09 12:06:30 +0900

    [D3] Title: N-Queen, Time: 74 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -5,51 +5,52 @@ class Solution {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
     private static int N;
-    private static boolean[] col;
-    private static boolean[] mainDiagonal;
-    private static boolean[] subDiagonal;
-    
-    private static int qcase;
+    private static int ALL_ONES;
+    private static int cnt;
     
 	public static void main(String args[]) throws Exception {
         StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine().trim());
 
 		for(int test_case = 1; test_case <= T; test_case++) {
-            qcase = 0;
+            cnt = 0;
             N = Integer.parseInt(br.readLine().trim());
-            col = new boolean[N+1];
-            mainDiagonal = new boolean[2*N+1];
-            subDiagonal = new boolean[2*N+1];
-                
-            setQueen(1);
+
+            // N=4일때 -> (1<<4)-1 = 15 (이진수: 1111)
+            // 1은 채워진 수
+            ALL_ONES = (1<<N)-1;
+            
+            setQueen(0, 0, 0);
             
             sb.append("#").append(test_case).append(" ")
-                .append(qcase).append("\n");
+                .append(cnt).append("\n");
 		}
         System.out.print(sb);
 	}
     
-    private static void setQueen(int row) {
-        if(row>N) {
-            qcase++;
+    private static void setQueen(int cols, int mainDiag, int subDiag) {
+        if(cols == ALL_ONES) { // 모든 열에 퀸이 다 채워진 경우
+            cnt++;
             return;
         }
+        // 현재 행에서 퀸을 놓을 수 있는 모든 위치 (1인 비트가 가능한 위치)
+        int available = ALL_ONES & ~(cols | mainDiag | subDiag);
         
-        for(int c=1; c<=N; c++) {
-            if(!isAvailable(row, c)) continue;
-            col[c] = mainDiagonal[row-c+N] = subDiagonal[row+c] = true;
-            setQueen(row+1);
-            col[c] = mainDiagonal[row-c+N] = subDiagonal[row+c] = false;
+        while(available > 0) {
+            // 가장 오른쪽 1(가능한 위치) 추출
+            int p = available & -available;
+            
+            // 다음 행으로 이동(대각선은 1비트씩 시프트)
+            setQueen(cols | p, (mainDiag | p) << 1, (subDiag | p) >> 1);
+            
+            // 처리한 위치 비트 제거
+            available = available & (available-1);
+            
         }
-    }
-    
-    private static boolean isAvailable(int r, int c) {
-        return !col[c] && !mainDiagonal[r-c+N] && !subDiagonal[r+c];
     }
 }
 
 /**
  * 시간복잡도: O(N!)
- * 원리: backtracking, 가지치기(pruning), 퀸의 위치 인덱스 값의 합 혹은 차를 활용한 연산 최적화
+ * 원리: 비트마스킹!! 
  */
